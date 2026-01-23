087a52a712c8ed730128343af426623481d6c4e1
euchxn <iyuchan90@gmail.com>
2026-01-23 10:57:22 +0900

    login

--- auth.ts
+++ auth.ts
@@ -1,10 +1,7 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { prisma } from "@/lib/db";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: PrismaAdapter(prisma),
   providers: [Google],
   session: {
     strategy: "jwt",
