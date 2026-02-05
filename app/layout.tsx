fd8bc60b934a2c46bda57777bd5ecbb04fbe10d2
euchxn <iyuchan90@gmail.com>
2026-02-05 22:03:53 +0900

    feat: Microsoft Clarity 추적 스크립트 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,6 +49,17 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             `,
           }}
         />
+        <Script
+          id="clarity-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "vcmr84rz47");`,
+          }}
+        />
       </head>
       <body className="min-h-screen bg-gray-50 dark:bg-gray-900 transition-colors">
         {/* Google Tag Manager (noscript) */}
